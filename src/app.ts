19b9c7efd596f37c6617ef141668390fbd4130f0
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2024-02-08 20:07:14 -0300

    fix: path api documentation

--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,11 @@ import { handleErrors } from "./middlewares/handleErrors.middleware";
 import { sessionRoutes } from "./routes/session.route";
 import { clientsRoutes } from "./routes/clients.routes";
 import { contactsRoutes } from "./routes/contacts.routes";
-
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger_output.json')
-
-const options = {
-    swaggerDefinition: {
-        openapi: "3.0.0",
-        info: {
-            title: "API de Exemplo",
-            version: "1.0.0",
-            description: "Uma API de exemplo documentada com Swagger",
-        },
-    },
-    apis: ["./routes/*.ts"],
-};
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger_output.json")
 
 export const app = express();
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(cors());
 app.use(express.json());
 
