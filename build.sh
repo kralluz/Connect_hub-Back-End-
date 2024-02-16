4e6ae855345fc5848576ab5b41898bee95d7fb98
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2024-02-16 17:10:36 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 #!/usr/bin/env bash
 set -o errexit
 # exit on error
-yarn install
+npm install
 npm install @types/express @types/cors @types/jsonwebtoken @types/bcryptjs @types/node --save-dev
 
-yarn build
+npm build
 
 # Gerar o cliente Prisma
 npx prisma generate
