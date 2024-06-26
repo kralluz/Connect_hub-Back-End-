96aba3c497de6acb66e45fefdd96abaafb008a2c
Carlos Henrique klz <97392902+kralluz@users.noreply.github.com>
2024-06-25 21:46:09 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,20 @@ import { sessionRoutes } from "./routes/session.route";
 import { clientsRoutes } from "./routes/clients.routes";
 import { contactsRoutes } from "./routes/contacts.routes";
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger_output.json")
+const swaggerDocument = require("../swagger_output.json");
 
 export const app = express();
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-app.use(cors());
+
+// Configuração do CORS
+const corsOptions = {
+  origin: 'https://connect-hub-front-end.vercel.app',
+  optionsSuccessStatus: 200 // Algumas versões do navegador exigem isso para compatibilidade
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/session", sessionRoutes);
 app.use("/clients", clientsRoutes);
 app.use("/contacts", contactsRoutes);
